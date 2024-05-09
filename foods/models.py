41b87f1f170d094d55c6b1f7d8a289af0fbff236
alirezaalireza77 <alireza.hashemian77@gmail.com>
2024-05-09 13:42:10 +0330

    add food models and gitignore to project

---
+++ foods/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class Food(models.Model):
+    """The purpose of this class is giving some information about foods"""
+
+    name = models.CharField(_("نام"),max_length=50)
+    description = models.CharField(_("توضیحات"), max_length=1000)
+    rate = models.IntegerField(_("امتیاز"))
+    price = models.IntegerField(_("قیمت"),default=0)
+    time = models.IntegerField(_("زمان آماده شدن"))
+    published_date = models.DateField(_("تاریخ انتشار"), auto_now=False, auto_now_add=True)
+    photo = models.ImageField(_("عکس"),Upload_to='foods/', blank=True)
+
+    def __str__(self):
+        return self.name
